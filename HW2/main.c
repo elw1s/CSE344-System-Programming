14b431f5ef1d2a1b0b6c01f09eb827aea4e3d801
Arda Kılıç <ardakilicbm@gmail.com>
2023-04-14 15:23:14 +0300

    makefile added

--- HW2/main.c
+++ HW2/main.c
@@ -14,6 +14,8 @@
 #define MAX_ARG_LENGTH 128
 #define MAX_COMMANDS 64
 
+int redirectionHappend = 0;
+
 
 char * parse_filename(char *command){
 
@@ -27,7 +29,8 @@ char * parse_filename(char *command){
     }
     if(strcmp(delimiter, " ") == 0) return NULL;
 
-    char * token = strtok(command, delimiter);
+    //char * token = strtok(command, delimiter);
+    strtok(command, delimiter);
     char * fileName = strtok(NULL, delimiter);
 
     while(*fileName == ' '){
@@ -35,20 +38,43 @@ char * parse_filename(char *command){
     }
 
     return fileName;
-    
 }
 
-int shell(char* command, int pipes[][2], int pipeIndex, int flag, FILE* logfile){
+int is_redirection_available(char * command){
+    for(int i = 0; i < strlen(command); i++){
+        if(command[i] == '<' || command[i] == '>')
+            return 1;
+    }
+    return 0;
+}
+
+int is_redirection_output(char * command){
+    for(int i = 0; i < strlen(command); i++){
+        if(command[i] == '>')
+            return 1;
+    }
+    return 0;
+}
+
+int is_redirection_input(char * command){
+    for(int i = 0; i < strlen(command); i++){
+        if(command[i] == '<')
+            return 1;
+    }
+    return 0;
+}
+
+
+int shell(char* command, int pipes[2],int * write_fd, int * read_fd, FILE* logfile, int commandIndex, int totalProcesses){
 
     //FLAGS:    0 start
     //          1 middle
     //          2 end
 
     sigset_t blockMask, origMask;
-    struct sigaction saIgnore, saOrigQuit, saOrigInt, saDefault;
+    struct sigaction saIgnore, saOrigQuit, saOrigInt;
     pid_t childPid;
     int status, savedErrno;
-
 
     sigemptyset(&blockMask);
     /* Block SIGCHLD */
@@ -62,33 +88,35 @@ int shell(char* command, int pipes[][2], int pipeIndex, int flag, FILE* logfile)
     sigaction(SIGINT, &saIgnore, &saOrigInt);
     sigaction(SIGQUIT, &saIgnore, &saOrigQuit);
 
+    /* If it is not the last process, then pipe should be created. For N process, N-1 pipe will be created */
+    if(commandIndex + 1 < totalProcesses){
+        pipe(pipes);
+        *write_fd = pipes[1];
+    }
+    else{
+        *write_fd = STDOUT_FILENO;
+    }
     
     switch (childPid = fork()) {
         case -1: /* fork() failed */
                 status = -1;
                 break;
-                /* Carry on to reset signal attributes */
-            case 0: /* Child: exec command */
-                if(flag == 0){
-                dup2(pipes[pipeIndex][1], STDOUT_FILENO);
+            case 0: /* Child */
+
+                if(*read_fd != STDIN_FILENO){
+                    dup2(pipes[0], STDIN_FILENO);
+                    close(pipes[0]);
                 }
-                else if(flag == 1){
-                    dup2(pipes[pipeIndex-1][0], STDIN_FILENO);
-                    dup2(pipes[pipeIndex][1], STDOUT_FILENO);
+                
+                if(*write_fd != STDOUT_FILENO){
+                    dup2(pipes[1], STDOUT_FILENO);
+                    close(pipes[1]);
                 }
-                else if(flag == 2){
-                    dup2(pipes[pipeIndex - 1][0], STDIN_FILENO);
-                }
+
                 execl("/bin/sh", "sh", "-c", command, (char *) NULL);
+                    perror("Execution of command failed\n");
                 exit(1);
-                /* We could not exec the shell */
-            default: /* Parent: wait for our child to terminate */
-                if (waitpid(childPid, &status, 0) == -1) {
-                    if (errno != EINTR) {
-                    status = -1;
-                    break;
-                }
-        }
+            default: 
                 break;
     }
 
@@ -98,19 +126,39 @@ int shell(char* command, int pipes[][2], int pipeIndex, int flag, FILE* logfile)
     sigaction(SIGQUIT, &saOrigQuit, NULL);
     errno = savedErrno;
 
-    if(flag == 0){
+    /* if(flag == 0 && pipes[pipeIndex][1] != STDOUT_FILENO){
         close(pipes[pipeIndex][1]);
     }
-    else if(flag == 1){
+    else if(flag == 1 && pipes[pipeIndex-1][0] != STDIN_FILENO && pipes[pipeIndex][1] != STDOUT_FILENO){
         close(pipes[pipeIndex-1][0]);
         close(pipes[pipeIndex][1]);
     }
-    else if(flag == 2){
+    else if(flag == 2 && pipes[pipeIndex - 1][0] != STDIN_FILENO){
         close(pipes[pipeIndex - 1][0]);
-    } 
-    fprintf(logfile, "Command %d: %s, PID: %d\n", pipeIndex, command, childPid);
-    return status;
+    }  */
 
+    if(*read_fd != STDIN_FILENO){
+        close(pipes[0]);
+    }
+
+    if(*write_fd != STDOUT_FILENO){
+        close(pipes[1]);
+    }
+
+    if(commandIndex + 1 < totalProcesses){
+        *read_fd = pipes[0];
+        close(pipes[1]);
+    }
+
+    fprintf(logfile, "Command %d: %s, PID: %d\n", commandIndex, command, childPid);
+
+    if (waitpid(childPid, &status, 0) == -1) {
+        if (errno != EINTR) {
+            status = -1;
+        }
+    }
+
+    return status;
 
 }
 
@@ -123,7 +171,6 @@ void parse_command(char* command, char** commands, char* args[MAX_COMMANDS][MAX_
         i++;
     }
     commands[i] = NULL;
-
 }
 
 int main(){
@@ -136,8 +183,9 @@ int main(){
 
         printf("(ARTER)$:");
         fflush(stdout);
+        setbuf(stdin, NULL);
         fgets(input, MAX_INPUT_SIZE, stdin);
-        input[strcspn(input, "\n")] = 0; // remove trailing newline
+        input[strcspn(input, "\n")] = 0;
 
         if (strcmp(input, ":q") == 0) {
             break;
@@ -150,9 +198,10 @@ int main(){
             numberOfCommands ++;
         }
 
-        int pipes[numberOfCommands - 1][2];
+        /* int pipes[numberOfCommands - 1][2];
         for(int k = 0; k < numberOfCommands - 1; k++)
-            pipe(pipes[k]);
+            if(pipe(pipes[k])== -1)
+                perror("pipe error"); */
 
         time_t t = time(NULL);
         struct tm tm = *localtime(&t);
@@ -165,18 +214,19 @@ int main(){
             exit(EXIT_FAILURE);
         }
 
+        int pipes[2];
+        int write_fd , read_fd = STDIN_FILENO;
+
         for(int i = 0; i < numberOfCommands; i++){
-            int flag;
+            /* int flag;
             if(i == 0) flag = 0;
             else if(i == numberOfCommands - 1) flag = 2;
-            else flag = 1;
-            shell(commands[i], pipes, i, flag, logfile);
+            else flag = 1; */
+            printf("write_fd = %d, read_fd = %d, i = %d\n", write_fd, read_fd, i);
+            shell(commands[i], pipes, &write_fd, &read_fd, logfile, i, numberOfCommands);
         }       
 
-        // log child process IDs and commands to file
-        /* for(int i = 0; i < numberOfCommands; i++){
-            fprintf(logfile, "Command %d: %s, PID: %d\n", i+1, commands[i], getpid());
-        } */
+
         fprintf(logfile, "------------------------------\n");
         fclose(logfile);
 
